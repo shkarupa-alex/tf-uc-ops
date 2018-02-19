6edbb68ff496f4c3525b605e22d740143a29b03b
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-02-19 17:46:06 +0300

    Правки в кофиге Travis

--- build-wheels.sh
+++ build-wheels.sh
@@ -3,12 +3,11 @@ set -e -x
 
 
 # Install a system package required by ours
-echo "$TRAVIS_OS_NAME"
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-get -qq update
+echo "$OS_NAME"
+if [[ "$OS_NAME" == "linux" ]]; then
     sudo apt-get install -y g++ icu-devtools
 fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+if [[ "$OS_NAME" == "osx" ]]; then
     brew update
     brew install icu4c
     export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
@@ -17,7 +16,7 @@ fi
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/python" /io/setup.py install_egg_info
+    (cd /io/; "${PYBIN}/python" setup.py egg_info)
     "${PYBIN}/pip" install -r /io/tfucops.egg-info/requires.txt
     make -f/io/Makefile build
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
