ce1ef97ad3934f5c3a4e5132ba68c99e21595bf7
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-02-19 17:26:18 +0300

    Обновлена версия

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name='tfucops',
-    version='1.0.1',
+    version='1.0.2',
     description='Unicode string OPs for TensorFlow',
     url='https://github.com/shkarupa-alex/tfucops',
     author='Shkarupa Alex',
