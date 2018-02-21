7d4c15bb7d6ded5e3c6c534cba7d2a376b562580
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-02-21 12:35:50 +0300

    Обновлена версия

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='tfucops',
-    version='1.0.6',
+    version='1.1.0',
     description='Unicode string OPs for TensorFlow',
     url='https://github.com/shkarupa-alex/tfucops',
     author='Shkarupa Alex',
