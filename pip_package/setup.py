5afc58df6c02f2b86fae11af91223f0f3ff2bcc9
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-08-03 08:42:01 +0300

    Tuned linux release and building

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
 
-__VERSION__ = '2.0.1'
+__VERSION__ = '2.0.2'
 
 
 class BinaryDistribution(Distribution):
