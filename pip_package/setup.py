6c188d9435b874b44ef9b3010fcbf5725a51e6a1
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-10-03 16:11:20 +0300

    Still failing

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
 
-__VERSION__ = '2.1.1'
+__VERSION__ = '2.1.2'
 
 
 class BinaryDistribution(Distribution):
